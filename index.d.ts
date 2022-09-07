fffd477522e7bc2bf53afadc446a0ca62858bfde
AdrianCraft07 <proadrian684@gmail.com>
2022-09-06 23:56:02 -0500

    types

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import getParam from "./getParam";
+import getClass from "./getClass";
+import colors from "./colors";
+import input from "./input";
+import load from "./load";
+import uuid from "./uuid";
+import min from "./min";
+export { getParam, getClass, colors, input, load, uuid, min };
